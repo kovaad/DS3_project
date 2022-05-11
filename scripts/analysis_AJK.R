dbc3a0454c734c62805d8ed3239b884915806939
kovaad <kovacs.adam@krtk.hu>
2022-05-11 16:59:55 +0200

    preparatory steps

--- scripts/analysis_AJK.R
+++ scripts/analysis_AJK.R
@@ -12,13 +12,30 @@ rm(list = ls())
 if (!require("pacman")) {
   install.packages("pacman")
 }
-pacman::p_load(dplyr,tidyverse, quanteda, quanteda.textstats, ggrepel,text2vec, topicmodels,ggfortify,ggwordcloud)
+pacman::p_load(dplyr,tidyverse)#, quanteda, quanteda.textstats, ggrepel,text2vec, topicmodels,ggfortify,ggwordcloud)
 
 #check out custom theme
 source("theme_adam.R")
 
 #read in data
 df <- read_csv("data/full_df_correct.csv")
+
+
+#read in data
+df <- read_csv("data/full_df_correct.csv")
+
+#create target variable of before and after the elections
+#rename body for text
+#remove any unnecessary whitespces
+df <- df |> 
+  mutate(
+    label = ifelse(dates < ymd("2022-4-3"), "előtte", "utána") 
+  ) |> 
+  rename(text = body) |> 
+  mutate(
+    text = stringr::str_trim(text),
+    text = stringr::str_squish(text)
+  )
 
 
 #sentiment analysis
